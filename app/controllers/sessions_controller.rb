db8549dfa0531f75611c05b4c064761685961da6
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-07-01 12:05:19 +0300

    compete assignment

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+    def create 
+        user  = User.find_by(username: params[:username])
+        session[:user_id] = user.id 
+        render json: user
+    end
+
+    def destroy 
+        user  = User.find_by(username: params[:username])
+        # session[:user_id] = user.id 
+        session[:user_id] = nil
+        head :no_content
+    end
+end
